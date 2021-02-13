092319582791bd4969dfe53de6ec48f70a8dfd13
Seyoun Lee <54106189+youn16@users.noreply.github.com>
2021-02-13 15:23:31 +0900

    Update SY_B1931.cpp

--- Algo/2021-02/2013/SY_B1931.cpp
+++ Algo/2021-02/2013/SY_B1931.cpp
@@ -1,53 +1,42 @@
 # baekjoon 2003 : 회의실 배정
 # solved by JY
 # DATE : 2021.02.13
-
-#include <iostream> 
+#include <iostream>
+#include <vector>
 #include <algorithm>
- 
+
 using namespace std;
- 
-typedef struct meeting {
-    int start;
-    int fin;
-}meeting;
- 
-meeting* meeting_arr[100001];
-int total_meeting = 0;
-int now_num = 0 ;
- 
-bool cmp_EDGE(meeting* a, meeting* b) { return a->fin < b->fin; }
-bool cmp_EDGE_2(meeting* a, meeting* b) { return a->start < b->start; }
- 
+vector<pair<long long, long long> > v;
 int main() {
-    int total_num = 0;
-    cin >> total_num;
- 
-    for (int i = 0; i < total_num; i++) {
-        int start, end;
-        cin >> start >> end;
- 
-        meeting_arr[i] = (meeting*)malloc(sizeof(meeting));
-        meeting_arr[i]->start = start;
-        meeting_arr[i]->fin = end;
-    }
- 
-    sort(meeting_arr, meeting_arr + total_num, cmp_EDGE);
- 
-    for (int i = 0; i < total_num; i++) {
-        if (now_num <= meeting_arr[i]->start){
- 
-            int j = i+1; 
-            while( (  j < total_num )  &&   ( meeting_arr[i]->fin == meeting_arr[j]->fin )  ){
-                j++;
-            }
-            sort(meeting_arr+i, meeting_arr + j  , cmp_EDGE_2);
- 
-            now_num = meeting_arr[i]->fin;
-            total_meeting++;
-        }
-    }
- 
-    cout << total_meeting;
-    return 0;
+	ios::sync_with_stdio(false);
+	int n;
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		long long x, y;
+		cin >> x >> y;
+		v.push_back(make_pair(y, x));
+	}
+
+	sort(v.begin(), v.end());
+	int cnt = 1;
+	int i = 1;
+	long long now_end = v[0].first;
+	while(true) {
+		long long next_start, next_end;
+		next_start = v[i].second;
+		next_end = v[i].first;
+		if (next_start >= now_end) {
+			now_end = next_end;
+			cnt++;
+		}
+		i++;
+
+		if (i >= n) break;
+	}
+	cout << cnt;
+
+	return 0;
 }
