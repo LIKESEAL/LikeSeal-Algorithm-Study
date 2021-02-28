7a2e7f7873e3dd86f4675fbd9479092db7e04fec
Seyoun Lee <54106189+youn16@users.noreply.github.com>
2021-02-28 23:38:08 +0900

    Update SY_B1788.cpp

--- Algo/2021-02/0227/SY_B1788.cpp
+++ Algo/2021-02/0227/SY_B1788.cpp
@@ -1,23 +1,54 @@
-# baekjoon 1788 : 피보나치 수으 확장
-# solved by SY
-# DATE : 2021.02.27
-# DP 사용
+/*
+baekjoon 1788 : 피보나치 수으 확장
+solved by SY
+DATE : 2021.02.27
+DP 사용
+*/
 
-#include <iostream> 
-using namespace std; 
-long long dp[1000001] = { 0, 1, }; 
-int main(void) { 
-  int n, k; 
-  cin >> n; 
-  if (n < 0) k = -n; else k = n; 
-  for (int i = 2; i <= k; i++) dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000000; 
-  if (n > 0) cout << 1 << '\n' << dp[k] % 1000000000; 
-  else if (n == 0) cout << "0\n0"; 
-  else 
-  { 
-    if ((-n) % 2 == 0) cout << -1 << '\n' << dp[k] % 1000000000; 
-    else cout << 1 << '\n' << dp[k] % 1000000000; 
-  } 
-  cout << '\n'; 
-return 1;
+#include <iostream>
+#define mod 1000000000
+using namespace std;
+
+int dp[1000001]={0,1,0};
+
+int main(){
+    int input;
+    int index;
+    cin >> input;
+    if(input<0)
+    {
+        index = input *(-1);
+    }
+    else
+    {
+        index = input;
+    }
+    if(index == 0 )
+    {
+        cout << 0;
+        cout << "\n";
+        cout << 0;
+        return 0;
+    }
+    for(int i = 2; i <= index; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % mod;
+    }
+    dp[index] = dp[index] % mod;
+    if(input < 0)//음수의 경우
+    {
+        if(index % 2 == 0)
+        {
+            cout << -1;
+            cout << "\n";
+            cout << dp[index];
+            return 0;
+        }
+    }
+    
+    cout << 1;
+    cout << "\n";
+    cout << dp[index];
+    
+
+    return 0;
 }
